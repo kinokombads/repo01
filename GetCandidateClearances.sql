1ea5ac34dedaa6ee5d99d717d1c156c8367793fb
kinokombads <42504644+kinokombads@users.noreply.github.com>
2020-02-10 00:46:48 +0800

    fixed the stored procs

--- GetCandidateClearances.sql
+++ GetCandidateClearances.sql
@@ -1,26 +1,35 @@
-DROP PROCEDURE IF EXISTS GetCandidateClearances;
-DELIMITER $$;
+drop procedure if exists GetCandidateClearances;
+delimiter $$;
 
-CREATE PROCEDURE GetCandidateClearances(
-    -- IN intId INT,
-    IN strPosition VARCHAR(100),
-    IN strGrade VARCHAR(100)
+create procedure GetCandidateClearances(
+    in intPositionId int,
+    in strPosition varchar(100),
+    in intGradeId int,
+    in strGrade varchar(100)
 )
-BEGIN
+begin
+    select
+        ifnull(a.candidateClearanceId, 0) AS CandidateClearanceId,
+        ifnull(a.positionId, 0) AS PositionId,
+        ifnull(b.title, '') AS PositionName,
+        ifnull(a.gradeId, 0) AS GradeId,
+        ifnull(c.title, '') AS GradeName,
+        ifnull(a.Active, 0) AS Active
+    from candidateClearances a
+        left join positions b on a.positionId = b.positionId
+        left join grades c on a.gradeId = c.gradeId
+    where (a.positionId = intPositionId or intPositionId = 0)    
+    and (b.title like concat('%', strPosition, '%') or strPosition = '')
+    and b.statId = 1
+    and (a.gradeId = intGradeId or intGradeId = 0)
+    and (c.title like concat('%', strGrade, '%') or strGrade = '')
+    and c.statId = 1;
+end;
 
-    SELECT
-        IFNULL(a.candidateClearanceId, 0) AS CandidateClearanceId,
-        IFNULL(a.positionId, 0) AS PositionId,
-        IFNULL(b.title, '') AS PositionName,
-        IFNULL(a.gradeId, 0) AS GradeId,
-        IFNULL(c.title, '') AS GradeName,
-        IFNULL(a.Active, 0) AS Active
-    FROM candidateclearances a
-        LEFT JOIN positions b ON a.positionId = b.positionId
-        LEFT JOIN grades c ON a.GradeId = c.gradeId
-    WHERE 
-    -- (a.candidateClearanceId = intId OR intId = 0) AND
-     (b.title LIKE CONCAT('%', strPosition, '%') OR strPosition = '')
-    AND (c.title LIKE CONCAT('%', strGrade, '%') OR strGrade = '');
+    -- WHERE 
+    -- -- (a.candidateClearanceId = intId OR intId = 0) AND
+    --  (b.title LIKE CONCAT('%', strPosition, '%') OR strPosition = '')
+    -- AND b.statId = 1    
+    -- AND (c.title LIKE CONCAT('%', strGrade, '%') OR strGrade = '')
+    -- AND c.statId = 1;
     
-END;
--- GetVoterClearances.sql
+++ GetVoterClearances.sql
@@ -2,25 +2,26 @@ drop procedure if exists GetVoterClearances;
 delimiter $$;
 
 create procedure GetVoterClearances(
-    -- in intId int,
+    in intPositionId int,
     in strPosition varchar(100),
+    in intGradeId int,
     in strGrade varchar(100)
 )
 begin
-
     select
-        ifnull(a.voterClearanceId, 0) as VoterClearanceId,
-        ifnull(a.positionId, 0) as PositionId,
-        ifnull(b.title, '') as PositionName,
-        ifnull(a.gradeId, 0) as GradeId,
-        ifnull(c.title, '') as GradeName,
-        ifnull(a.Active, 0) as Active
-    from voterclearances a
+        ifnull(a.voterClearanceId, 0) AS VoterClearanceId,
+        ifnull(a.positionId, 0) AS PositionId,
+        ifnull(b.title, '') AS PositionName,
+        ifnull(a.gradeId, 0) AS GradeId,
+        ifnull(c.title, '') AS GradeName,
+        ifnull(a.Active, 0) AS Active
+    from voterClearances a
         left join positions b on a.positionId = b.positionId
-        left join grades c on a.GradeId = c.gradeId
-    where 
-    -- (a.voterClearanceId = intId or intId = 0) and 
-    (b.title like concat('%', strPosition, '%') or strPosition = '')
-    and (c.title like concat('%', strGrade, '%') or strGrade = '');
-    
+        left join grades c on a.gradeId = c.gradeId
+    where (a.positionId = intPositionId or intPositionId = 0)    
+    and (b.title like concat('%', strPosition, '%') or strPosition = '')
+    and b.statId = 1
+    and (a.gradeId = intGradeId or intGradeId = 0)
+    and (c.title like concat('%', strGrade, '%') or strGrade = '')
+    and c.statId = 1;
 end;
